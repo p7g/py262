97b19da26e4c988b0dbe9c6166964e7b194eef97
Patrick Gingras <775.pg.12@gmail.com>
2019-07-25 21:53:32 -0400

    fixed issues with schrodinger_property

--- py262/utils/schrodinger_property.py
+++ py262/utils/schrodinger_property.py
@@ -1,11 +1,16 @@
+from weakref import WeakSet
+
+
 class schrodinger_property(property):  # pylint: disable=invalid-name
     '''A class property that cannot be changed once it has been observed'''
-    observed = False
+    def __init__(self, *args, **kwargs):
+        self.observed = WeakSet()
+        super().__init__(*args, **kwargs)
 
-    def __get__(self, *args, **kwargs):
-        self.observed = True
-        return super().__get__(*args, **kwargs)
+    def __get__(self, instance, *args, **kwargs):
+        self.observed.add(instance)
+        return super().__get__(instance, *args, **kwargs)
 
     def __set__(self, obj, value):
-        assert not self.observed
+        assert obj not in self.observed
         return super().__set__(obj, value)
