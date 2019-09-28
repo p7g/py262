83b71fd065d29cf16c6f0924135b0ac24b020d9d
Patrick Gingras <775.pg.12@gmail.com>
2019-09-28 00:24:53 -0400

    fixed nameerror and improved type_of function

--- py262/abstract_ops/value.py
+++ py262/abstract_ops/value.py
@@ -1,19 +1,21 @@
 from typing import TYPE_CHECKING, Union
 
 from py262.environment import AbstractEnvironment
-from py262.value import BooleanValue, NullValue, UndefinedValue
+from py262.value import BooleanValue, NullValue, ObjectValue, UndefinedValue
 
 if TYPE_CHECKING:
-    from py262.value import Value
+    from py262.value import Value  # noqa: W0611
 
 
-def type_of(v: Union[Value, AbstractEnvironment]) -> str:
+def type_of(v: Union['Value', AbstractEnvironment]) -> str:
     if isinstance(v, BooleanValue):
         return 'boolean'
     if isinstance(v, NullValue):
         return 'null'
     if isinstance(v, UndefinedValue):
         return 'undefined'
+    if isinstance(v, ObjectValue):
+        return 'object'
     if isinstance(v, AbstractEnvironment):
         return 'environment'
-    raise NotImplementedError()
+    raise ValueError(v)
